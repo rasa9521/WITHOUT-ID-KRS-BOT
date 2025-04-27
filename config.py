98b2ad006b83f662b5e7e560ad8d3553ef0a6ecc
rasa9521 <rsrasasingh@gmail.com>
2025-04-27 21:47:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "27900743"
+api_hash = "ebb06ea8d41420e60b29140dcee902fc"
+bot_token = "7870546403:AAHDmw1SYKR-KI9sWuVt5di88x3W6XSMSN8"
+auth_users = [7455272483]
