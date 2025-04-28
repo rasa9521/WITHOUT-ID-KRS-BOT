bdd37933157df7914806074bd66c68291ffa43f6
rasa9521 <rsrasasingh@gmail.com>
2025-04-28 08:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "27900743")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "8194163011:AAH-mTmPMDuHFlKgc9xizS1zBKoBJc_eQgE") #Replace with your bot token
+api_id = "27900743"
+api_hash = "ebb06ea8d41420e60b29140dcee902fc"
+bot_token = "8194163011:AAH-mTmPMDuHFlKgc9xizS1zBKoBJc_eQgE"
 auth_users = [7455272483]
