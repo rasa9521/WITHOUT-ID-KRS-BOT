3ec1a6aba21b8c6459008337115bae0ca8248786
rasa9521 <rsrasasingh@gmail.com>
2025-04-28 14:58:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-from os import environ
-
 api_id = "27900743"
 api_hash = "ebb06ea8d41420e60b29140dcee902fc"
-bot_token = "8194163011:AAH-mTmPMDuHFlKgc9xizS1zBKoBJc_eQgE"
+bot_token = "7850491064:AAHw_g0tbwUWzStzu-BHN4TKiZR8SgMDyrY"
 auth_users = [7455272483]
