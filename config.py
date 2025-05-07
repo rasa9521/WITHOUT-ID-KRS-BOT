1cb7f2b5731c345056d4c3da813fdd0101064700
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-07 07:42:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27900743"
 api_hash = "ebb06ea8d41420e60b29140dcee902fc"
 bot_token = "7850491064:AAHw_g0tbwUWzStzu-BHN4TKiZR8SgMDyrY"
-auth_users = [7455272483 , 7804396225]
+auth_users = [7455272483 , 7804396225 , 7993588999]
