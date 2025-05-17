c6b8b6e5f13809fb2d052dff81391cdc4a0c259f
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-17 11:04:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ api_id = "27900743"
 api_hash = "ebb06ea8d41420e60b29140dcee902fc"
 bot_token = "7850491064:AAHw_g0tbwUWzStzu-BHN4TKiZR8SgMDyrY"
 auth_users = [7455272483 , 7804396225]
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002500107960"))
